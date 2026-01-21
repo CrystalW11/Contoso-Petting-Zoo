1352970823ba52b6337e7a80a27a1bda9f0dd7db
CrystalW11 <crystal.warmack@gmail.com>
2026-01-21 11:30:32 -0700

    updated folders

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Contoso-Petting-Zoo
+
+![alt text](image.png)
+
+
+### Terminal Output: 
+
+![alt text](image-1.png)
+
+
+
--- obj/Debug/net9.0/51-Contoso Petting Zoo.AssemblyInfo.cs
+++ obj/Debug/net9.0/51-Contoso Petting Zoo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("51-Contoso Petting Zoo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55ac5f1c6158669b395d8a554e6fb807776ec33f")]
 [assembly: System.Reflection.AssemblyProductAttribute("51-Contoso Petting Zoo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("51-Contoso Petting Zoo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
